1c95375c9543b3f1730f913e7fe0a3889bf2b3cf
Dmitry Groshkov <bygeorg2623@gmail.com>
2024-09-20 13:33:23 +0300

    Выполнил первое домашнее задание

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = "Первая переменная";
+let screens = "Вторая переменная";
+let screenPrice = "Третья переменная";
+let rollback = "Четвертая переменная";
+let fullPrice = "Пятая переменная";
+let adaptive = "Шестая переменная";
+
+alert('Привет, преподаватель!')
+console.log("Как дела?");
